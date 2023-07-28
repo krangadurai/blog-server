eadfbf94e45402aee15f6c300be727ed692b684a
krangadurai <rangadurai27@gmail.com>
2023-07-28 16:40:07 +0530

    localhost

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb+srv://rangadurai27:QZ9G90wCtVcki45Z@cluster0.ir97p41.mongodb.net/Blog",
+  //url:"mongodb://localhost:27017"
+   url:"mongodb+srv://rangadurai27:QZ9G90wCtVcki45Z@cluster0.ir97p41.mongodb.net/Blog"
 };
